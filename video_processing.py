95f7cba3c9ab7362e69f7a298fd8046e91a087b7
Rahul Banerjee <rbanerjee28@gmail.com>
2021-12-17 14:03:27 +0100

    added save feature

--- video_processing.py
+++ video_processing.py
@@ -8,7 +8,8 @@ import cmapy
 import json
 import os
 
-def apply_transformations(image, required_shape):
+def apply_transformations(frame, required_shape, original_shape, T):
+    image = temperature_to_image(frame, original_shape, T[0], T[1])
     width = required_shape[0]
     height = required_shape[1]
     image = cv2.applyColorMap(image, cmapy.cmap('jet'))
@@ -19,10 +20,18 @@ def apply_transformations(image, required_shape):
 def temperature_to_image(frame, shape, T_min, T_max):
     image = np.uint8((frame - T_min) * 255 / (T_max-T_min))
     image.shape = (shape[1], shape[0])
+    
     return image
 
-def get_video_sequential(device, original_shape, required_shape):
+def save_image(image):
+    filename = "thermal_image_" + str(time.time()) + ".jpg"
+    print("Saving Image...")
+    cv2.imwrite(filename, image)
+    print("Image Saved.")
+
+def process_video_sequential(device, original_shape, required_shape):
     frame = np.zeros((original_shape[0] * original_shape[1], ))
+    latency = staticmethod
     print("Runnig Sequential Version")
 
     while True:
@@ -31,18 +40,21 @@ def get_video_sequential(device, original_shape, required_shape):
         t_start = time.time()
         T_min = np.min(frame)
         T_max = np.max(frame)
-        image = temperature_to_image(frame, original_shape, T_min, T_max)
-        image = apply_transformations(image, required_shape)
-        cv2.imshow('Thermal Image', image)
+        image = apply_transformations(frame, required_shape, original_shape, (T_min, T_max))
         time_end = time.time()
-        print("FPS : ", 1 / (time_end-time_start))
+        text = f"T_min : {T_min:.1f}C ; T_max : {T_max:.1f}C ; FPS : {1 / (time_end-time_start):.1f} ; Interpolation : INTER_CUBIC"
+        cv2.putText(image, text, (30,18), cv2.FONT_HERSHEY_SIMPLEX, .4, (255,255,255), 1)
+        cv2.imshow('Thermal Image', image)
 
-        if cv2.waitKey(1) == ord('q'):
-            break   
+        key = cv2.waitKey(1)
+        if key == ord('q'):        
+            break
+        elif key == ord('s'):
+            save_image(image)
 
     cv2.destroyAllWindows()
 
-def get_video_multithreaded(device, original_shape, required_shape):
+def process_video_multithreaded(device, original_shape, required_shape):
     frame = np.zeros((original_shape[0] * original_shape[1], ))
     thread_num = cv2.getNumberOfCPUs()
     print("Runnig Paralle Version ; Total CPUs : ", thread_num)
@@ -50,25 +62,28 @@ def get_video_multithreaded(device, original_shape, required_shape):
     pending_task = deque()
 
     while True:
-        time_start = time.time()
         while len(pending_task) > 0 and pending_task[0].ready():
             res = pending_task.popleft().get()
-            cv2.imshow('Thermal Image', res)
+            text = f"T_min : {T_min:.1f}C ; T_max : {T_max:.1f}C ; FPS : {1 / (time_end-time_start):.1f} ; Interpolation : INTER_CUBIC"
+            cv2.putText(res, text, (30,18), cv2.FONT_HERSHEY_SIMPLEX, .4, (255,255,255), 1)
+            cv2.imshow('Thermal Image (multithreaded)', res)
 
         if len(pending_task) < thread_num:
+            time_start = time.time()
             device.getFrame(frame)
-            t_start = time.time()
             T_min = np.min(frame)
             T_max = np.max(frame)
-            image = temperature_to_image(frame, original_shape, T_min, T_max)
-            task = pool.apply_async(apply_transformations, (image.copy(), required_shape))
+            T = (T_min, T_max)
+            task = pool.apply_async(apply_transformations,
+                                    (frame.copy(), required_shape, original_shape, T))
             pending_task.append(task)
+            time_end = time.time()
         
-        time_end = time.time()
-        print("FPS : ", 1 / (time_end-time_start))
-        
-        if cv2.waitKey(1) == ord('q'):
-            break   
+        key = cv2.waitKey(1)
+        if key == ord('q'):        
+            break
+        elif key == ord('s'):
+            save_image(res)
 
     cv2.destroyAllWindows()
 
@@ -77,23 +92,22 @@ if __name__ == '__main__':
         print("Reading Config File")
         with open("config.json", 'r') as jsonfile:
             config = json.load(jsonfile)
-        print(config)
     else:
         raise FileNotFoundError
     
     frequency = config['frequency']
     scale = config['scale']
     refresh_rate = config['refresh_rate']
-    version = config['version']
+    parallel = config['parallel']
     
-    print('Starting Program')
+    print('Starting Program. At any point, press the S key to save an image or Q to exit.')
     i2c = busio.I2C(board.SCL, board.SDA, frequency=frequency)
     mlx = adafruit_mlx90640.MLX90640(i2c)
     mlx.refresh_rate = eval(config['refresh_rate'])
     mlx_shape = (config['mlx_width'], config['mlx_height'])
     mlx_interp_shape = (mlx_shape[0] * scale, mlx_shape[1] * scale)
     
-    if version == 'parallel':
-        get_video_multithreaded(mlx, mlx_shape, mlx_interp_shape)
+    if parallel == True:
+        process_video_multithreaded(mlx, mlx_shape, mlx_interp_shape)
     else:
-        get_video_sequential(mlx, mlx_shape, mlx_interp_shape)
+        process_video_sequential(mlx, mlx_shape, mlx_interp_shape)
