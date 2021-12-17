a2385d56ec4a76286217b6c71f9a320ae35893c3
Rahul Banerjee <rbanerjee28@gmail.com>
2021-12-17 14:14:49 +0100

    added docstrings

--- video_processing.py
+++ video_processing.py
@@ -9,6 +9,18 @@ import json
 import os
 
 def apply_transformations(frame, required_shape, original_shape, T):
+    """
+    Method to convert the raw frame into temperature map and apply interpolations
+
+    Args:
+        frame ([mlx dataframe]): [Thermal dataframe from the MLX device]
+        required_shape ([tuple]): [Final shape]
+        original_shape ([tuple]): [Original shape of the MLX image]
+        T ([tuple]): [Contains minimum and maximum temperature of the frame]
+
+    Returns:
+        [image]: [final transformed image from the raw frame]
+    """
     image = temperature_to_image(frame, original_shape, T[0], T[1])
     width = required_shape[0]
     height = required_shape[1]
@@ -18,18 +30,44 @@ def apply_transformations(frame, required_shape, original_shape, T):
     return image
 
 def temperature_to_image(frame, shape, T_min, T_max):
+    """
+    Converts the temperature field of the MLX frame to an image array
+
+    Args:
+        frame ([mlx dataframe]): [Thermal dataframe from the MLX device]
+        shape ([tuple]): [Final shape]
+        T_min ([float]): [Minimum temperature in the frame]
+        T_max ([float]): [Maximum temperature in the frame]
+
+    Returns:
+        [image]: [Image from the raw temperature frame]
+    """
     image = np.uint8((frame - T_min) * 255 / (T_max-T_min))
     image.shape = (shape[1], shape[0])
     
     return image
 
 def save_image(image):
+    """
+    Saves image
+
+    Args:
+        image ([np.array]): [thermal image]
+    """
     filename = "thermal_image_" + str(time.time()) + ".jpg"
     print("Saving Image...")
     cv2.imwrite(filename, image)
     print("Image Saved.")
 
 def process_video_sequential(device, original_shape, required_shape):
+    """
+    Performs the thermal image processing using 1 CPU core
+
+    Args:
+        device ([MLX Device]): [MLX Camera identifier]
+        original_shape ([tuple]): [Original shape of the MLX image]
+        required_shape ([tuple]): [Final shape]
+    """
     frame = np.zeros((original_shape[0] * original_shape[1], ))
     latency = staticmethod
     print("Runnig Sequential Version")
@@ -55,6 +93,14 @@ def process_video_sequential(device, original_shape, required_shape):
     cv2.destroyAllWindows()
 
 def process_video_multithreaded(device, original_shape, required_shape):
+    """
+    Performs the thermal image processing using multiple CPU cores
+
+    Args:
+        device ([MLX Device]): [MLX Camera identifier]
+        original_shape ([tuple]): [Original shape of the MLX image]
+        required_shape ([tuple]): [Final shape]
+    """
     frame = np.zeros((original_shape[0] * original_shape[1], ))
     thread_num = cv2.getNumberOfCPUs()
     print("Runnig Paralle Version ; Total CPUs : ", thread_num)
